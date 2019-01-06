d2f6fe07882bfe0ebfe0dd52c064cf43ed8e9763
JakobWonisch <jakob.wonisch@gmail.com>
2019-01-06 21:04:57 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,13 +5,18 @@ $(function(){
 	
   $('#world-map').vectorMap({map: 'world_mill'});
   
-  $.ajax({
-	  url: "jakobwonisch.github.io/data.xlsx",
-	  success: function(data)
-	  {
-		loadData(data, dataLoaded);
-	  }
-  })
+  //load data from excel sheet
+  var oReq = new XMLHttpRequest();
+	oReq.open("GET", "/data.xlsx", true);
+	oReq.responseType = "arraybuffer";
+
+	oReq.onload = function(oEvent) {
+	  var arrayBuffer = oReq.response;
+
+		loadData(arrayBuffer, dataLoaded);
+	};
+
+	oReq.send();
 });
 
 
