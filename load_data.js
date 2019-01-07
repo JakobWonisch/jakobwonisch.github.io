7b178fd38257c779249114431cb6e6e350986e8d
JakobWonisch <jakob.wonisch@gmail.com>
2019-01-07 05:30:45 +0100

    Major Update
    Development almost complete.

--- load_data.js
+++ load_data.js
@@ -1,8 +1,19 @@
 var callback;
 
+function loadSummaries()
+{
+	$.ajax({
+    url:'summaries.html',
+    success: function (data){
+		//console.log(data);
+		summariesLoaded(data);
+    }
+  });
+}
+
 function loadData(file, callback) {
 	
-	alert("loading data"+file);
+	//alert("loading test data"+file);
 	
 	this.callback=callback;
 	
@@ -18,9 +29,13 @@ function loadData(file, callback) {
 		// Here is your object
 		var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
 		var json_object = JSON.stringify(XL_row_object);
-		console.log(json_object);
+		var array_object=JSON.parse(json_object);
+		
+		array_object.sort(compare);
+		
+		//console.log(array_object);
 
-		callback(json_object);
+		callback(array_object);
 	  })
 
 	};
@@ -30,4 +45,12 @@ function loadData(file, callback) {
 	};
 
 	reader.readAsBinaryString(file);
+}
+
+function compare(a,b) {
+  if (a.Name < b.Name)
+    return -1;
+  if (a.Name > b.Name)
+    return 1;
+  return 0;
 }
--- main.js
+++ main.js
@@ -1,10 +1,38 @@
-
+let pageCountries=[],
+	pageSummaries={};
 
 $(function(){
 	//dom loaded
 	
-  $('#world-map').vectorMap({map: 'world_mill'});
+ // $('#world-map').vectorMap({map: 'world_mill'});
   
+   map = new jvm.Map({
+    container: $('#world-map'),
+    map: 'world_mill',
+	backgroundColor: "#7bb3d1",
+	regionStyle: {
+			initial: {
+				fill: '#424242',
+				"fill-opacity": 1,
+				stroke: 'none',
+				"stroke-width": 0,
+				"stroke-opacity": 1
+				},
+			hover: {
+				fill: 'white',
+				"fill-opacity": 0.8,
+				cursor: 'mouse'
+			},
+			selected: {
+				fill: '#ff9b30'
+			},
+			selectedHover: {
+			}
+		}
+  });
+  
+ // map.setSelectedRegions(highlights);
+ 
   //load data from excel sheet
   var oReq = new XMLHttpRequest();
 	oReq.open("GET", "/data.xlsx", true);
@@ -18,10 +46,124 @@ $(function(){
 	};
 
 	oReq.send();
+	
+	//setup page left and right
+	$("#page_left").click(function(){
+		changePage(-1);
+		updateHighlights();
+	});
+	$("#page_right").click(function(){
+		changePage(1);
+		updateHighlights();
+	});
 });
 
+function updateHighlights()
+{
+	//console.log(pageCountries[curIndex]);
+	map.clearSelectedRegions();
+	map.setSelectedRegions(pageCountries[curIndex]);
+}
+
+function summariesLoaded(data)
+{
+	let el=$("<html></html>");
+	el.html(data);
+	
+	let counter=0;
+	
+	el.find("table.c58 td").each(function(){
+		//let title=$(this).find("span.c10.c8,span.c9.c7").first().text().trim();
+		let title=$(this).find("span").first().text().trim();
+		
+		$(this).find("span").first().addClass("orgTitle");
+		
+		//console.log(title +" exists: "+(title in pageSummaries));
+		//console.log(title +" exists: "+(title in pageSummaries));
+		if(title in pageSummaries)
+		{
+			pageSummaries[title].html(getImg(title)+$(this).html());
+		
+			counter++;
+		}
+	});
+	
+	//console.log(pageSummaries["Agora"]);
+	console.log(counter);
+}
 
 function dataLoaded(result)
 {
-	alert("got result");
+	//remove all unwanted rows
+	for(let i=result.length-1; i>=0; i--)
+	{
+		if(result[i].Name=="Name")
+			result.splice(i, 1);
+	
+	}
+	
+	//put all countries in pageCountries
+	pageCountries=[];
+	
+	for(let i=0; i<result.length; i++)
+	{
+		let cs=[];
+		
+		if(typeof result[i]["undefined"]!=="undefined")
+		{
+			let temp=result[i]["undefined"].split(",");
+			
+			//convert to country codes
+			for(let j=0; j<temp.length; j++)
+			{
+				let code=countryNameToCode(temp[j].trim());
+				
+				if(Array.isArray(code))
+				{
+					for(let u=code.length; u>=0; u--)
+					{
+						if(code[u]==""||!(code[u] in jvmCountries))
+							code.splice(u, 1);
+					}
+					cs=cs.concat(code);
+				}else
+				{
+					if(code!=""&&code in jvmCountries)
+						cs.push(code);
+				}
+			}
+			//console.log(cs);
+		}		
+		
+		pageCountries.push(cs);
+	}
+	
+	//alert("got result");
+	let container=$("#data_container");
+
+	//create all views and add to list
+	for(let i=0; i<result.length; i++)
+	{
+		let page=$("<div></div>");
+		
+		page.addClass("page");
+		
+		//console.log(result[company].Name);
+		
+		page.css("display","none");
+		
+		page.html(getImg(result[i].Name)+result[i].Name);
+		
+		//page.prop("id", "id"+result[i].Name);
+		
+		pageSummaries[result[i].Name.trim()]=page;
+		
+		container.append(page);
+	}
+	
+	loadSummaries();
+	
+	initPageHandler();
+	pageChanged();
+	updateHighlights();
 }
